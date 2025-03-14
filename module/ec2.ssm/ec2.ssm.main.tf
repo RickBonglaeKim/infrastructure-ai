f915df94486364398bb767fd3ec8f3c91506712e
Iscream-Rick <rick.kim@i-screamarts.com>
2025-03-14 18:58:34 +0900

    Cleaned up ec2 settings for the SSM.

--- module/ec2.ssm/ec2.ssm.main.tf
+++ module/ec2.ssm/ec2.ssm.main.tf
@@ -1,28 +1,14 @@
 
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-
-  owners = ["amazon"]
-  filter {
-    name = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-}
-
-
 resource "aws_instance" "ec2_ssm" {
-  ami = data.aws_ami.amazon_linux_2.id
+  ami = local.ec2-ssm-ami
   instance_type = local.instance-type
   iam_instance_profile = aws_iam_instance_profile.ssm.name
   vpc_security_group_ids = [data.terraform_remote_state.network.outputs.security-group-ec2_ssm.id]
   subnet_id = data.terraform_remote_state.network.outputs.subnet-public["c"].id
 
-  lifecycle {
-    ignore_changes = [ami]
-  }
-
   tags = {
-    name = local.ec2-ssm-name
-    environment = var.environment
+    Name = local.ec2-ssm-name
+    Environment = var.environment
   }
+  
 }
