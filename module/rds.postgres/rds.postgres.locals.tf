553ac01294908f463f186f816db1b5e6dbba08d7
Iscream-Rick <rick.kim@i-screamarts.com>
2025-03-14 16:49:20 +0900

    Removed the RDS module.

--- module/rds.postgres/rds.postgres.locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  postgreSQL = {
-    engin = "postgres"
-    version = "17.2"
-    identifier = "ai-${var.environment}"
-  }
-}
--- module/rds.postgres/tds.postgres.subnet-group.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_db_subnet_group" "postgres" {
-  name = "postgres-${var.environment}"
-  subnet_ids = [for subnet in data.terraform_remote_state.network.outputs.subnet-database : subnet.id]
-}
