563151c9ec400241f7a94dc7dcde67ac9fd72d0e
Iscream-Rick <rick.kim@i-screamarts.com>
2025-03-28 18:20:30 +0900

    Updated security group.

--- module/vpc/vpc.security_group.ec2-sketch.tf
+++ module/vpc/vpc.security_group.ec2-sketch.tf
@@ -9,6 +9,14 @@ resource "aws_security_group" "ec2-sketch" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "ec2-sketch-7504" {
+  security_group_id = aws_security_group.ec2-sketch.id
+  cidr_ipv4 = aws_vpc.this.cidr_block
+  from_port = 7504
+  ip_protocol = "tcp"
+  to_port = 7504
+}
+
 resource "aws_vpc_security_group_egress_rule" "ec2-sketch-all" {
   security_group_id = aws_security_group.ec2-sketch.id
   cidr_ipv4 = local.cidr.all
